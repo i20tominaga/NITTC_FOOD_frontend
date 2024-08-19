10aa9e872ed6416d562ae43cc5adf162d7ae40ef
tominaga <zmstmb2804@ymobile.ne.jp>
2024-08-19 16:33:52 +0900

    ヘッダーの作成完了

---
+++ src/components/Header.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { AlignLeft } from 'lucide-react';
+
+
+export function Header() {
+    return (
+        <header className="p-4 flex items-center justify-between">
+            {/* シートをトリガーするボタン */}
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button variant="outline">
+                        <AlignLeft size={24} />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Edit profile</SheetTitle>
+                        <SheetDescription>
+                            Make changes to your profile here. Click save when you are done.
+                        </SheetDescription>
+                    </SheetHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-right">
+                                Name
+                            </Label>
+                            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                                Username
+                            </Label>å
+                            <Input id="username" value="@peduarte" className="col-span-3" />
+                        </div>
+                    </div>
+                    <SheetFooter>
+                        <SheetClose asChild>
+                            <Button type="submit">Save changes</Button>
+                        </SheetClose>
+                    </SheetFooter>
+                </SheetContent>
+            </Sheet>
+
+            <div className="text-center">
+                <h1 className="text-center flex-grow">NITTC-FOOD</h1>
+            </div>
+            <div className="flex items-center space-x-4">
+
+                <Button>Click me</Button>
+            </div>
+        </header>
+    )
+}
