59cbb460db6ffecbd24416d23e8ea76e46a64ec9
tominaga <zmstmb2804@ymobile.ne.jp>
2024-08-22 00:02:54 +0900

    デプロイ時のエラー訂正4

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function orderPage() {
+export default function orderPage() {
     return (
         <div className="p-4">
             <h3 className="text-lg font-bold">注文</h3>
--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function ticketPage() {
+export default function ticketPage() {
     return (
         <div className="p-4">
             <h3 className="text-lg font-bold">整理券予約</h3>
